bc21b829031f3a8b95f8f8402b25a93766569dae
andreamgrs <andrea.munozgr@gmail.com>
2025-08-11 05:07:46 +0000

    Adding main and script 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+ * selection_sort - function that sorts an array of integers in
+ * ascending order using the selection sort algorithm.
+ *
+ * @array: is a pointer to the first element in the array.
+ * @size: number of elements in the array.
+ *
+ * Return: nothing.
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t array_size;
+	size_t min_num, tmp;
+	size_t cont = 0;
+	
+	array_size = 0;
+	while (array_size < size - 1)
+	{
+		min_num = array_size;
+
+		cont = min_num + 1;
+		while (cont < size)
+		{
+			if(array[cont] < array[min_num])
+			{
+				min_num = cont;
+			}
+			cont = cont + 1;
+		}
+
+		tmp = array[array_size];
+		array[array_size] = array[min_num];
+		array[min_num] = tmp;
+		print_array(array, size);
+
+	array_size = array_size + 1;
+	}
+}
