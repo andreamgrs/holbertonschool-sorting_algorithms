9ee08fe9633ebccdbb7e1dd04c137875ac465801
andreamgrs <andrea.munozgr@gmail.com>
2025-08-11 05:39:59 +0000

    Adding main and script 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -32,10 +32,14 @@ void selection_sort(int *array, size_t size)
 			cont = cont + 1;
 		}
 
-		tmp = array[array_size];
-		array[array_size] = array[min_num];
-		array[min_num] = tmp;
-		print_array(array, size);
+		if (min_num != array_size)
+		{
+
+			tmp = array[array_size];
+			array[array_size] = array[min_num];
+			array[min_num] = tmp;
+			print_array(array, size);
+		}
 		array_size = array_size + 1;
 	}
 }
