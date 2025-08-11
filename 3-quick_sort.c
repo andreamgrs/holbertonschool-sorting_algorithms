554d4a580e437ad08fcc63f753c223f7bdb3efe2
root <andrea.munozgr@gmail.com>
2025-08-11 12:33:44 +0000

    Adding main and script 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,48 @@
 #include "sort.h"
 #include <stdlib.h>
+
+void swap(int *first, int *last)
+{
+	int tmp = *first;
+	*first = *last;
+	*last = tmp;
+
+}
+
+int partition(int *array, size_t size, int min, int max)
+{
+	int pivot_value, cont, value_to_swap;
+       
+	pivot_value = array[max];
+	value_to_swap = min;
+
+	cont = min;
+	while (cont < max)
+	{
+		if(array[cont] <= pivot_value)
+		{
+			swap(&array[value_to_swap], &array[cont]);
+			value_to_swap++;
+		}
+		cont = cont + 1;
+	}
+	swap(&array[value_to_swap], &array[max]);
+	print_array(array, size);
+	return (value_to_swap);
+
+}
+
+void quick_sort_recursion(int *array, size_t size, int min, int max)
+{
+	int pivot;
+
+	if (min < max)
+	{
+		pivot = partition(array, size, min, max);
+		quick_sort_recursion(array, size, min, pivot - 1);
+		quick_sort_recursion(array, size, pivot + 1, max);
+	}
+}
 /**
  * quick_sort - function that sorts an array of integers in
  * ascending order using the quick sort algorithm.
@@ -12,5 +55,5 @@
  */
 void quick_sort(int *array, size_t size)
 {
-
+	quick_sort_recursion(array, size, 0, size - 1);
 }
